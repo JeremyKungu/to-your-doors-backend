56fdcfcace493b89de3c880ae83d55fa81d40f2a
JeremyKungu <jeremikungu27@gmail.com>
2023-01-27 11:29:04 +0300

    added validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ApplicationRecord
     has_secure_password
     has_many :orders
+
+    validates :username, presence: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
+    validates :email, presence: true
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,9 @@ class Meal < ApplicationRecord
     has_many :restaurant_meals
     has_many :restaurants, through: :restaurant_meals
 
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :category, presence: true
+    validates :img_url, presence: true
 end
