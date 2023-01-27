9093532e785e7aea23cfd36d31c4edb4fd00a449
JeremyKungu <jeremikungu27@gmail.com>
2023-01-27 11:11:16 +0300

    added authorizations

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,6 @@
 class MealsController < ApplicationController
-
+    skip_before_action :authorized, only:[:index, :show]
+    
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
